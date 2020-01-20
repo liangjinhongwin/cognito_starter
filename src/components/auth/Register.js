528d1ba38956ce9ba5ca62b8534e05b07d383405
Jinhong Liang <liangjinhongwin@hotmail.com>
2020-01-20 13:47:12 -0800

    Added register and login

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import FormErrors from "../FormErrors";
 import Validate from "../util/Validation";
+import { Auth } from 'aws-amplify';
 
-class Register extends Component { 
+class Register extends Component {
   //state variables for form inputs and errors
-    state = {
+  state = {
     username: "",
     email: "",
     password: "",
     confirmpassword: "",
     errors: {
       blankfield: false,
-      matchedpassword: false
+      matchedpassword: false,
+      cognito: null
     }
   }
 
@@ -19,7 +21,8 @@ class Register extends Component {
     this.setState({
       errors: {
         blankfield: false,
-        matchedpassword: false
+        matchedpassword: false,
+        cognito: null
       }
     });
   }
@@ -37,6 +40,33 @@ class Register extends Component {
       });
     }
     //Integrate Cognito here on valid form submission
+    //Take the state variables to pass to the signUp method
+    //we added email as a required field and this needs to be
+    //passed to the api as an attribute.
+    const { username, email, password } = this.state;
+    try {
+      const signUpResponse = await Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email: email
+        }
+      });
+      console.log(signUpResponse);
+      //redirect to Welcome page if registration is successful
+      this.props.history.push("/welcome");
+    } catch (error) {
+      //check if error has a message property, if not add one.
+      let err = null;
+      !error.message ? err = { "message": error } : err = error;
+      //set form error as a cognito error
+      this.setState({
+        errors: {
+          ...this.state.errors,
+          cognito: err
+        }
+      });
+    }
   };
 
   onInputChange = event => {
@@ -55,8 +85,8 @@ class Register extends Component {
           <form onSubmit={this.handleSubmit}>
             <div className="field">
               <p className="control has-icons-left">
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="text"
                   id="username"
                   placeholder="Enter username"
@@ -70,8 +100,8 @@ class Register extends Component {
             </div>
             <div className="field">
               <p className="control has-icons-left">
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="email"
                   id="email"
                   placeholder="Enter email"
@@ -85,8 +115,8 @@ class Register extends Component {
             </div>
             <div className="field">
               <p className="control has-icons-left">
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="password"
                   id="password"
                   placeholder="Password"
@@ -100,8 +130,8 @@ class Register extends Component {
             </div>
             <div className="field">
               <p className="control has-icons-left">
-                <input 
-                  className="input" 
+                <input
+                  className="input"
                   type="password"
                   id="confirmpassword"
                   placeholder="Confirm password"
